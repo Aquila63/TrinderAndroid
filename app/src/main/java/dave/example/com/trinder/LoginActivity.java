49969347fdb2eac458a405057be03593c7aa3f0a
islamso <islamso@tcd.ie>
2015-03-02 14:16:30 +0000

    updated facebook login

--- app/src/main/java/dave/example/com/trinder/LoginActivity.java
+++ app/src/main/java/dave/example/com/trinder/LoginActivity.java
@@ -10,15 +10,24 @@ import android.view.Menu;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+
+import com.facebook.widget.LoginButton;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.widget.LoginButton.UserInfoChangedCallback;
+import com.facebook.model.GraphUser;
+
+
 
 public class LoginActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
 
-    //@InjectView(R.id.cardView) CardView cardView;
+    @InjectView(R.id.facebookButton) LoginButton facebookLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +41,31 @@ public class LoginActivity extends ActionBarActivity {
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout),toolbar);
 
-        // todo
+        facebookLoginButton.setReadPermissions(Arrays.asList("public_profie"));
+        facebookLoginButton.setUserInfoChangedCallback(new UserInfoChangedCallback() {
+            @Override
+            public void onUserInfoFetched(GraphUser user) {
+                if (user != null) {
+                    Log.d("Log in", "You are currently logged in as " + user.getName());
+                } else {
+                    Log.d("Log in", "You are not logged in.");
+                }
+            }
+        });
 
     }
+
+    private Session.StatusCallback statusCallback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            if (state.isOpened()) {
+                Log.d("LoginActivity", "Facebook session opened.");
+            } else if (state.isClosed()) {
+                Log.d("LoginActivity", "Facebook session closed.");
+            }
+        }
+    };
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,5 +73,7 @@ public class LoginActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+
 }
 
