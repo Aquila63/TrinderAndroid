f26f2e7ecfa7685558de9b34484d4dc6efd1ea01
islamso <islamso@tcd.ie>
2015-03-03 14:37:08 +0000

    updated verify email activity

--- app/src/main/java/dave/example/com/trinder/VerifyEmailActivity.java
+++ app/src/main/java/dave/example/com/trinder/VerifyEmailActivity.java
@@ -1,7 +1,49 @@
 package dave.example.com.trinder;
 
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 /**
  * Created by Dave on 03/03/15.
  */
-public class VerifyEmailActivity {
+public class VerifyEmailActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
+    @InjectView(R.id.emailField) TextView emailField;
+    @InjectView(R.id.submitButton) Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_email);
+        ButterKnife.inject(this);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+        return true;
+    }
+
+
+
 }
