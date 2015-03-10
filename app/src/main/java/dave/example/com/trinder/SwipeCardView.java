6819f2dbe8f776d9891164fe3ae7dcca023d5e89
Niall Paterson <npaters@tcd.ie>
2015-03-10 13:53:02 +0000

    Update SwipeCardView.java

--- app/src/main/java/dave/example/com/trinder/SwipeCardView.java
+++ app/src/main/java/dave/example/com/trinder/SwipeCardView.java
@@ -1,7 +1,6 @@
 public class SwipeCardView extends CardView {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-        Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.card_view, container, false);
         return view;
     }
