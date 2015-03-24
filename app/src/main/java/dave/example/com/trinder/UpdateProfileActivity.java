9e6aae612300ff67c01ef274f2a80ad4c6070252
burkegavan <burkega@tcd.ie>
2015-03-24 13:57:41 +0000

    Update UpdateProfileActivity

--- app/src/main/java/dave/example/com/trinder/UpdateProfileActivity.java
+++ app/src/main/java/dave/example/com/trinder/UpdateProfileActivity.java
@@ -1,6 +1,9 @@
 package dave.example.com.trinder;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,6 +11,11 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -18,8 +26,12 @@ import butterknife.OnClick;
  */
 public class UpdateProfileActivity extends ActionBarActivity {
     private Toolbar toolbar;
+    private static final int REQUEST_CODE = 1;
+    private Bitmap bitmap;
     @InjectView(R.id.changePictureButton)
     Button changePictureButton;
+    @InjectView(R.id.profileImage)
+    ImageView profilePicture;
 
 
     @Override
@@ -38,9 +50,9 @@ public class UpdateProfileActivity extends ActionBarActivity {
     public void submit() {
         Intent intent = new Intent();
         intent.setType("image/*");
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent,"Select Picture"), 1);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent, REQUEST_CODE);
     }
 
 
@@ -48,5 +60,26 @@ public class UpdateProfileActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK)
+            try {
+                // We need to recyle unused bitmaps
+                if (bitmap != null) {
+                    bitmap.recycle();
+                }
+                InputStream stream = getContentResolver().openInputStream(
+                        data.getData());
+                bitmap = BitmapFactory.decodeStream(stream);
+                stream.close();
+                profilePicture.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
